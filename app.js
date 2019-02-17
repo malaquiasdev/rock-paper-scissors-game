8e4a0c17bccbc2c370ba56a9a6618a106ffd09e6
Mateus Malaquias <mateus.malaquias@globalhitss.com.br>
2019-02-17 08:09:20 -0300

    Created the logic that will choice the computer response

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const ROCK_CHOICE_DIV = document.getElementById('rock');
 const PAPER_CHOICE_DIV = document.getElementById('paper');
 const SCISSORS_CHOICE_DIV = document.getElementById('scissors');
 
+const getComputerRandomChoice = () => {
+  const choices = ['rock', 'paper', 'scissors'];
+  const randomChoiceIndex = Math.floor(Math.random() * 3);
+  return choices[randomChoiceIndex];
+};
+
 const main = () => {
   ROCK_CHOICE_DIV.addEventListener('click', () => console.log('rock'));
   PAPER_CHOICE_DIV.addEventListener('click', () => console.log('paper'));
