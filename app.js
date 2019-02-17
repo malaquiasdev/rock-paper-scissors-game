ece0de045b6f46b1966eb3a14c320be58765c719
Mateus Malaquias <mateus.malaquias@globalhitss.com.br>
2019-02-17 08:03:00 -0300

    Created the initial js script, now i can get all div values in the DOM and try the click event in the choices component

---
+++ app.js
@@ -0,0 +1,18 @@
+let userScore = 0;
+let computerScore = 0;
+
+const USER_SCORE_SPAN = document.getElementById('user-scored');
+const COMPUTER_SCORE_SPAN = document.getElementById('computer-scored');
+const SCORE_BOARD_DIV = document.getElementById('score-board');
+const RESULT_DIV = document.getElementById('result');
+const ROCK_CHOICE_DIV = document.getElementById('rock');
+const PAPER_CHOICE_DIV = document.getElementById('paper');
+const SCISSORS_CHOICE_DIV = document.getElementById('scissors');
+
+const main = () => {
+  ROCK_CHOICE_DIV.addEventListener('click', () => console.log('rock'));
+  PAPER_CHOICE_DIV.addEventListener('click', () => console.log('paper'));
+  SCISSORS_CHOICE_DIV.addEventListener('click', () => console.log('scissors'));
+};
+
+main();
